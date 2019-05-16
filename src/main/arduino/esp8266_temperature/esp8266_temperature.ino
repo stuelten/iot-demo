c97f48b47b8d5fab0388a26fbf80b4a1cf9c38dd
Timo Stülten <timo.stuelten@googlemail.com>
2019-05-16 17:31:11 +0200

    Add temperature driven snake controller.

--- src/main/arduino/esp8266_temperature/esp8266_temperature.ino
+++ src/main/arduino/esp8266_temperature/esp8266_temperature.ino
@@ -1,15 +1,25 @@
 /*
-  Basic ESP8266 MQTT example
+  ESP8266 temperature sensor
 */
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <DHT.h>
 
 // Update these with values suitable for your network.
 
 const char* ssid = "iot-wlan";
 const char* password = "iot-wlan";
 const char* mqtt_server = "192.168.100.122";
+
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
 
 // control snake
 char* SNAKE_TOPIC = "snake/0";
@@ -21,9 +31,14 @@ int direction = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
 long lastMsg = 0;
 char msg[50];
+
 int value = 0;
+
+float lastTemp;
+float lastHum;
 
 // LED pin
 #define D0 16
@@ -41,7 +56,6 @@ void ledOff() {
 }
 
 void setup_wifi() {
-
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -102,6 +116,11 @@ void setup() {
   // Initialize the BUILTIN_LED pin as an output
   pinMode(BUILTIN_LED, OUTPUT);
   Serial.begin(115200);
+
+  // Initialize device.
+  dht.begin();
+
+  // setup network
   setup_wifi();
   client.setServer(mqtt_server, 1883);
 }
@@ -117,11 +136,25 @@ void loop() {
   if (now - lastMsg > 500) {
     lastMsg = now;
 
-    direction++;
-    if (direction > 3)
-      direction = 0;
-
     char* payload;
+
+    float t = dht.readTemperature();
+    //float h = dht.readHumidity();
+
+    Serial.print("Temperature read: ");
+    Serial.print(t);
+    Serial.print(". Previous temperature: ");
+    Serial.println(lastTemp);
+
+    if (!isnan(t) && t - 0.1 > lastTemp) {
+      turnLeft();
+    } else if (!isnan(t) && t + 0.1 < lastTemp) {
+      turnRight();
+    } else {
+      Serial.println("Keep straight ahead!");
+    }
+    lastTemp = t;
+
     if (0 == direction) {
       payload = NORTH;
     } else if (1 == direction) {
@@ -140,4 +173,20 @@ void loop() {
     client.publish(SNAKE_TOPIC, payload);
     ledBlink();
   }
+}
+
+void turnLeft() {
+  direction++;
+  while (direction > 3) direction -= 4;
+
+  Serial.print("turnLeft: ");
+  Serial.println(direction);
+}
+
+void turnRight() {
+  direction--;
+  while (direction < 0) direction += 4;  
+
+  Serial.print("turnRight: ");
+  Serial.println(direction);
 }
